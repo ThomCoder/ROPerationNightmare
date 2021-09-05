be0e8755c4bd23f5230f52fb31df6eb7e5074fed
ThomCoder <43434564+ThomCoder@users.noreply.github.com>
2021-09-05 19:25:47 +0200

    Add separate printer functions
    Some systems have upwards growing stacks. In such cases a buffer
    overflow would not harm the stored return address as the buffer grows in
    the wrong direction.
    
    If a program is to be easily exploited in such cases a seperate function
    whose return address can be "pre spoiled" is helpful.
    See
    security.stackexchange.com/questions/44801/smashing-the-stack-if-it-grows-upwards
    for more info.

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+void print_it_out(char *s)
+{
+	printf("%s\n", s);
+}
+
 void echo(void)
 {
 	char buf[10];
 	scanf("%s", buf);
-	printf("%s\n", buf);
+	print_it_out(buf);
 }
 
 int main(int argc, char **argv)
